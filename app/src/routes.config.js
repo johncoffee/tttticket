22f0b17c27336fab3bd76d77a2ffb82b6f13f935
Julian <julian@contingent.dk>
2015-09-26 15:51:09 +0200

    first

---
+++ app/src/routes.config.js
@@ -0,0 +1,22 @@
+angular.module('app')
+.config(function($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'src/login/login.html',
+            resolve: {
+                // I will cause a 1 second delay
+                delay: function($q, $timeout) {
+                    var delay = $q.defer();
+                    $timeout(delay.resolve, 1000);
+                    return delay.promise;
+                }
+            }
+        })
+        .when('/Book/:bookId/ch/:chapterId', {
+            templateUrl: 'chapter.html',
+            controller: 'ChapterController'
+        });
+
+    // configure html5 to get links working on jsfiddle
+    $locationProvider.html5Mode(true);
+});
