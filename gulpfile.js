fafe39cc57faaaeab91fdb2e4b802bec1992c1a0
Julian <julian@contingent.dk>
2015-10-10 19:09:59 +0200

    bower n stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,19 @@ var inject = require('gulp-inject');
 var concat = require('gulp-concat');
 var angularTemplates = require('gulp-angular-templates');
 var watch = require('gulp-watch');
+var mainBowerFiles = require('main-bower-files');
 
 var js = [
     // vendor
-    "./app/bower_components/angular/angular.js",
-    "./app/bower_components/angular-animate/angular-animate.js",
-    "./app/bower_components/angular-aria/angular-aria.js",
-    "./app/bower_components/angular-material/angular-material.js",
-    "./app/bower_components/angular-route/angular-route.js",
+    //"./app/bower_components/angular/angular.js",
+    //"./app/bower_components/angular-animate/angular-animate.js",
+    //"./app/bower_components/angular-aria/angular-aria.js",
+    //"./app/bower_components/angular-material/angular-material.js",
+    //"./app/bower_components/angular-route/angular-route.js",
 
     // our stuff
     './app/src/**/*.js',
-];
-
-var css = [
-    './assets/**/*.css',
+    './app/assets/**/*.css',
 ];
 
 var handle = null;
@@ -33,7 +31,15 @@ gulp.task('watch', function () {
 });
 
 function devBuild() {
-    var sources = gulp.src(js, {read: false});
+    var bowerFiles = mainBowerFiles({
+        paths: {
+            bowerDirectory: './app/bower_components',
+            bowerrc: './bowerrc',
+            bowerJson: './bower.json'
+        }
+    });
+    console.log(bowerFiles);
+    var sources = gulp.src(bowerFiles.concat(js), {read: false});
     return gulp.src('./app/index.html')
         .pipe( inject(sources, {'ignorePath':'app'}))
         .pipe(gulp.dest('./app'));
