2138c54cace7bc787836f175f6f14abfdd2182f1
Julian <julian@contingent.dk>
2015-10-13 00:46:57 +0200

    pay flow stuff

--- app/src/pages/ticketsPage/ticketsPage.controller.js
+++ app/src/pages/ticketsPage/ticketsPage.controller.js
@@ -1,5 +1,29 @@
-function TicketsPageController($log) {
-
+function TicketsPageController($log, $window) {
+    this.tickets = [
+        {
+            name: "Nordic Game Jam 2016",
+            price: "DKK 700,00",
+        },
+    ];
+    
+    this.buyTicket = this.tickets[0];
+    
+    this.paymentOptions = [
+        {
+            name: "mobile pay",
+        },
+        {
+            name: "mobile pay",
+        }
+    ];
+    
+    this.print = function() {
+        $window.print();
+    }
 }
+
+TicketsPageController.prototype.buy = function (ticket) {
+    this.buyTicket = ticket;
+};
 
 angular.module("app").controller("TicketsPageController", TicketsPageController);
