88a2e4460cba1a5382f63e06e311697633f62eef
Julian <julian@contingent.dk>
2015-10-17 12:12:53 +0200

    fields..

--- app/src/pages/ticketsPage/ticketsPage.controller.js
+++ app/src/pages/ticketsPage/ticketsPage.controller.js
@@ -1,4 +1,6 @@
-function TicketsPageController($log, $window, $q) {
+function TicketsPageController($log, $window, $q, $timeout) {
+    var vm = this;
+
     this.tickets = [
         {
             name: "Nordic Game Jam 2016",
@@ -17,17 +19,43 @@ function TicketsPageController($log, $window, $q) {
         }
     ];
     
-    this.sendOff = function() {
-        
+    this.loadLast = function () {
+        buyAndAuth({
+            cardno: vm.buyTicket.cardno,
+            regno: vm.buyTicket.regno,
+
+            email: vm.buyTicket.email,
+        })
+        .then(function (result) {
+            vm.buyTicket.orderno = result.orderno;
+        });
     };
+    
+    function buyAndAuth() {
+        var deferred = $q.defer();
+        $timeout(function () {
+            var respMock = {
+                orderno: 42,
+            };
+            deferred.resolve(respMock);
+        }, 333);
+        
+        return deferred.promise;
+    }
     
     this.print = function() {
         $window.print();
-    }
+    };
+
+    this.reset();
 }
 
 TicketsPageController.prototype.buy = function (ticket) {
     this.buyTicket = ticket;
+};
+
+TicketsPageController.prototype.reset = function () {
+    this.buyTicket = angular.copy(this.tickets[0]);
 };
 
 angular.module("app").controller("TicketsPageController", TicketsPageController);
