af6d18ddac47029ece643f6585e77ff0c06ab037
Julian <julian@contingent.dk>
2015-10-22 00:53:02 +0200

    payment flow

--- app/src/pages/ticketsPage/ticketsPage.controller.js
+++ app/src/pages/ticketsPage/ticketsPage.controller.js
@@ -1,25 +1,20 @@
-function TicketsPageController($log, $window, $q, $timeout) {
+function TicketsPageController($log, 
+                               $http,
+                               $window,
+                               $q, 
+                               $timeout,
+                               Auth) {
     var vm = this;
-
+    var apiBase = "//localhost:8080/api/";
+    
+    vm.isLoggedIn = Auth.authenticated;
+    
+    this.step = 0;
+    
     this.tickets = [
         {
             name: "Nordic Game Jam 2016",
             price: "DKK 700,00",
-            
-            regno: 1234,
-            cardno: "1000 0000 0000 0008",
-            expm: 12,
-            expy: 12,
-            
-        },
-    ];
-    
-    this.paymentOptions = [
-        {
-            name: "mobile pay",
-        },
-        {
-            name: "VISA card",
         },
     ];
     
@@ -34,7 +29,6 @@ function TicketsPageController($log, $window, $q, $timeout) {
             vm.buyTicket.orderno = result.orderno;
         });
     };
-    
     function buyAndAuth() {
         var deferred = $q.defer();
         $timeout(function () {
@@ -50,14 +44,109 @@ function TicketsPageController($log, $window, $q, $timeout) {
     this.print = function() {
         $window.print();
     };
+    
+    var _state = {};
+    Object.defineProperty(this, "state", {
+        get: function () {
+            return _state;    
+        },    
+    });
+    
+    this.signUp = function(email, password) {
+        vm.buyTicket.wait = true;
+        $timeout(function () {
+            vm.buyTicket.wait = false;
+            vm.next();
+        }, 666);
+    };
+    
+    this.next = function () {
+        vm.step++;
+    };
+    
+    this.requestLink = function() {
+        vm.buyTicket.wait = true;
+
+        //test
+        //vm.qpLink = 42;
+        //vm.transID = 42;
+        //vm.buyTicket.wait = false;
+        //return;
+        
+        $http({
+            url: apiBase + "request.php",
+            method: "GET",
+            params: {
+                ticket_type: 2,
+            },
+            cache: false,
+            responseType: "application/json",
+        })
+        .success(function(response) {
+            vm.buyTicket.wait = false;
+                
+            console.debug(response);
+            if (response.error) {
+                console.warn(response);
+            }
+            else {
+                vm.qpLink = response.qpLink;    
+                vm.transID = response.transID;    
+            }
+        })
+        .error(function () {
+            vm.buyTicket.wait = false;
+            console.warn("sheit");
+        });    
+    };
+    
+    this.qpCheckout = function () {
+        window.open(vm.qpLink, "_blank");
+        vm.buyTicket.wait = true;
+        setTimeout(function () {
+            vm.pollStatus(vm.transID);    
+        }, 10 * 1000);
+        // test
+        //vm.onStatusResponse({payment: {state: "processed"} });
+    };
+    
+    var handle = null;
+    this.pollStatus = function (transID) {
+        clearTimeout(handle);
+        handle = setTimeout(function () {
+            vm.requestStatus(transID).success(vm.onStatusResponse);
+        }, 4333);
+    };
+    
+    this.onStatusResponse = function (response) {
+        console.debug(response);
+        var payment = response.payment;
+        switch (payment.state) {
+            case "initial":
+                vm.pollStatus(vm.transID);
+                break;
+            case "processed":
+                console.debug("processed!!");
+                vm.buyTicket.orderno = 42; // response....
+                vm.next();
+                break;
+        }
+    };
+
+    this.requestStatus = function (id) {
+        return $http({
+            url: apiBase + "status.php",
+            method: "GET",
+            params: {
+                id: id,
+            },
+            cache: false,
+            responseType: "application/json",
+        });
+    };
 
     vm.reset();
 }
-
-TicketsPageController.prototype.buy = function (ticket) {
-    this.step = 1;
-    this.buyTicket = ticket;
-};
 
 TicketsPageController.prototype.reset = function () {
     this.buyTicket = angular.copy(this.tickets[0]);
