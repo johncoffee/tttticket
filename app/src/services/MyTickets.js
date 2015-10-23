78962e8f24264769ea20e56e8cd2957398e815f9
Julian <julian@contingent.dk>
2015-10-23 18:25:14 +0200

    tester sees tickets

--- app/src/services/MyTickets.js
+++ app/src/services/MyTickets.js
@@ -1,31 +1,32 @@
 function MyTickets($q, Auth) {
     
-    Object.defineProperties(this, {
-        authenticated: {
-            get: function () {
-                return Auth.authenticated;
-            }
-        }
-    });
-    
     this.getMyTickets = function () {
         var tickets = [];
-        
-        if (this.authenticated) {
-            tickets.push({
+        if (Auth.test) {
+            tickets.push( new Ticket(1, {
                 name: "Nordic Game Jam",
-            });
-            tickets.push({
-                name: "Copenhagen games festival",
-            });
-            tickets.push({
-                name: "w00t",
-            });    
+            }));
+
+            tickets.push( new Ticket(2, {
+                name: "Beer ticket",
+            }));
         }
-        
         return $q.when(tickets);
     };
 }
+
+function Ticket(id, data) {
+    this.id = id;
+    if (data) {
+        this.hydrate(data);
+    }
+}
+
+Ticket.prototype.hydrate = function (data) {
+    for (var i in data) if (data.hasOwnProperty(i)) {
+        this[i] = data[i];    
+    }    
+};
 
 
 angular.module("app").service("MyTickets", MyTickets);
