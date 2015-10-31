5c11774d59c800241b25db597c40d50870f8be4e
Julian <julian@contingent.dk>
2015-11-01 00:00:38 +0100

    removed http config

--- app/src/app/http.config.js
+++
@@ -1,7 +0,0 @@
-
-angular.module('app')
-    .config(function($httpProvider) {
-        $httpProvider.defaults.responseType = "json";
-        
-        console.debug($httpProvider.defaults)
-    });
