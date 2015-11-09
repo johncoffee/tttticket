df02ac8bfc95f774f05e2c4862161a084091c38e
Julian <julian@contingent.dk>
2015-11-09 20:45:40 +0100

    theme

--- app/src/app/theming.config.js
+++ app/src/app/theming.config.js
@@ -3,19 +3,6 @@ angular
     .config(function($mdThemingProvider) {
         
         $mdThemingProvider.theme('default')
-            .primaryPalette('red')
+            .primaryPalette('teal')
             .accentPalette('orange');
-    })
-
-    .config(function($mdIconProvider){
-
-        $mdIconProvider
-            .defaultIconSet("./assets/svg/avatars.svg", 128)
-            .icon("menu"       , "./assets/svg/menu.svg"        , 24)
-            .icon("share"      , "./assets/svg/share.svg"       , 24)
-            .icon("google_plus", "./assets/svg/google_plus.svg" , 512)
-            .icon("hangouts"   , "./assets/svg/hangouts.svg"    , 512)
-            .icon("twitter"    , "./assets/svg/twitter.svg"     , 512)
-            .icon("phone"      , "./assets/svg/phone.svg"       , 512);
-
     });
