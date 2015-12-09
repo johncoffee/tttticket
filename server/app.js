2347a1ad8b27d9d268fccdb88618c85aaf164bc9
Julian <julian@contingent.dk>
2015-12-09 16:29:14 +0100

    serve static files

--- server/app.js
+++ server/app.js
@@ -5,35 +5,25 @@ var request = require('request');
 var app = express();
 
 var staticPath = __dirname + "/../app/";
-var apiHost = "http://localhost:8000";
+var apiHost = "http://localhost:8000/";
 
 //app.use(cors());
-
 // set request defaults
-request = request.defaults({
-    jar: request.jar(),
-    proxyHeaderWhiteList: ['Access-Control-Allow-Origin', 'Cookie']
-});
+//request = request.defaults({
+    //jar: request.jar(),
+    //proxyHeaderWhiteList: ['Access-Control-Allow-Origin', 'Cookie']
+//});
 
 process.on('uncaughtException', function(err) {
     console.log(err);
 });
 
+
+// static files
+
 app.use("/", express.static(staticPath, {
     etag: false,
 }) );
-
-app.all('/api/*', pipeApiRequest);
-
-function pipeApiRequest(req, res) {
-    var target = apiHost + req.url;
-    //console.log('piping request to:' + target);
-    var options = {
-        url: target,
-    };
-    res.contentType('json');
-    req.pipe(request(options)).pipe(res);
-}
 
 app.listen(3000, function() {
     console.log('Serving on port 3000');
